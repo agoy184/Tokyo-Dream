540fd0b3b89125bef231c1360bd3c442abf1eae0
Abel Goy <agoy@ucsc.edu>
2023-05-30 18:27:31 -0700

    Moving Player
    Player (placeholder asset) now moves when using WSAD

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,15 +4,22 @@ class Play extends Phaser.Scene {
     }
 
     preload() {
-
+        this.load.image('Grandpa', './assets/EmptyChibi.png')
     }
 
     create() {
-        // this.player = new Player(this, 200, 200, '', 0)
+        this.player = new Player(this, 200, 200, 'Grandpa', 0)
+
+        // Define keys
+        keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
     }
 
     update() {
-
+        this.player.update();
     }
 
 }   
