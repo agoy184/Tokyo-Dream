04e5480ade6fce1e7e73bf805bcbd82bde3a4def
Abel Goy <agoy@ucsc.edu>
2023-06-02 23:40:10 -0700

    Delayed follow (X)

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,11 +4,12 @@ class Play extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('Grandpa', './assets/EmptyChibi.png')
+        this.load.image('Person', './assets/EmptyChibi.png')
     }
 
     create() {
-        this.player = new Player(this, 200, 200, 'Grandpa', 0)
+        this.grandpa = new Player(this, 200, 200, 'Person', 0)
+        this.grandma = new Player(this, 240, 240, 'Person', 0)
 
         // Define keys
         keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
@@ -19,7 +20,12 @@ class Play extends Phaser.Scene {
     }
 
     update() {
-        this.player.update();
+        this.grandpa.update();
+        this.clock = this.time.delayedCall(3000, () => {
+            this.grandma.update();
+        }, null, this);
+
+
     }
 
 }   
