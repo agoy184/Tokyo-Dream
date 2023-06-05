9467d40d8bc36ff38d60da47ed158d393f91c46a
Marlene Inoue <mminoue@ucsc.edu>
2023-06-05 01:16:05 -0700

    added background image for first scene

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,9 +5,12 @@ class Play extends Phaser.Scene {
 
     preload() {
         this.load.image('Person', './assets/EmptyChibi.png')
+        this.load.image('background', './assets/arrival_and_neglect_background.png')
     }
 
     create() {
+        this.background = this.add.image(game.config.width / 2, game.config.height / 2, 'background').setOrigin(0.5, 0.5);
+
         this.grandpa = new Player(this, 200, 200, 'Person', 0)
         this.grandma = new Player(this, 240, 240, 'Person', 0)
 
