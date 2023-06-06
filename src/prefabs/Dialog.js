f7bc10b71b1a4f1db907f5286ea2473b0056baa4
Marlene Inoue <mminoue@ucsc.edu>
2023-06-06 09:35:06 -0700

    changed Dialog to a regular class

--- src/prefabs/Dialog.js
+++ src/prefabs/Dialog.js
@@ -1,19 +1,20 @@
-class Dialog extends Phaser.GameObjects.Sprite {
+class Dialog {
     constructor(scene, dialog, character) {
 
         let x = game.config.width / 2;
         let y = game.config.height / 2;
 
-        super(scene, x, y, character);
+//        super(scene, x, y, character);
 
         this.dialog = dialog;
         this.character = character;
 
-        scene.add.existing(this);
+        //scene.add.existing(this);
     }
 
     create() {
-//        scene.add.text(this.dialog);
+        console.log("*", this.dialog, "*");
+//        this.add.text(game.config.width / 3, game.config.height / 3, this.dialog);
     }
 
     update() {
