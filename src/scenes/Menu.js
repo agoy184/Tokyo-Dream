99ba64c1ece266e6895ff7740599f7d5511f6093
Marlene Inoue <mminoue@ucsc.edu>
2023-06-07 00:02:20 -0700

    added music credits to credit scene, added controls to menu scene

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -18,9 +18,10 @@ class Menu extends Phaser.Scene{
         }
 
         // menu text
-        this.add.text(game.config.width/2, game.config.height/4, 'Tokyo Dream', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2, 'Press SPACE to start', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, 3*game.config.height/4, 'Press D for credits', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/6, 'Tokyo Dream', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2.5, 'Controls\nWASD to move\nClick on sprites for dialog\nENTER for next scene', menuConfig).setAlign('center').setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/1.5, 'Press SPACE to start', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/1.25, 'Press D for credits', menuConfig).setOrigin(0.5);
 
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
