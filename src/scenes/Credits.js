e6cd26eed90886b07ff602b2d12c6f61451f4744
Abel Goy <agoy@ucsc.edu>
2023-06-13 14:21:04 -0700

    added credits for tilemap and Tiled

--- src/scenes/Credits.js
+++ src/scenes/Credits.js
@@ -25,6 +25,8 @@ class Credits extends Phaser.Scene {
         this.add.text(game.config.width/4, game.config.height/2.5, 'First Scene Music\nGreener Pastures by Darren Curtis | https://www.darrencurtismusic.com/\nMusic promoted by https://www.chosic.com/free-music/all/\nCreative Commons CC BY 3.0\nhttps://creativecommons.org/licenses/by/3.0/', menuConfig).setWordWrapWidth(game.config.width / 2).setAlign('center').setFontSize(10).setOrigin(0.5);
         this.add.text(game.config.width/1.35, game.config.height/2.5, 'Second Scene Music\nSakuya2 by PeriTune | http://peritune.com\nCreative Commons Attribution 3.0 Unported License\nhttps://creativecommons.org/licenses/by/3.0/deed.en_US\nMusic promoted by https://www.chosic.com/free-music/all/', menuConfig).setWordWrapWidth(game.config.width / 2.5).setAlign('center').setFontSize(10).setOrigin(0.5);
         this.add.text(game.config.width/4, game.config.height/1.5, 'Third Scene Music\nRain On The Window by Alex-Productions | https://onsound.eu/\nMusic promoted by https://www.free-stock-music.com\nCreative Commons / Attribution 3.0 Unported License (CC BY 3.0)\nhttps://creativecommons.org/licenses/by/3.0/deed.en_US', menuConfig).setWordWrapWidth(game.config.width / 2.5).setAlign('center').setFontSize(10).setOrigin(0.5);
+        this.add.text(game.config.width/1.35, game.config.height/1.5, 'Tileset used: https://rekkimaru.itch.io/32-pixel-art-japanese-themed-textures\nCreated in Tiled (https://www.mapeditor.org/)', menuConfig).setWordWrapWidth(game.config.width / 2.5).setAlign('center').setFontSize(10).setOrigin(0.5);
+
 
         this.add.text(game.config.width/2, 8*game.config.height/9, 'Press S to return to the title screen menu', menuConfig).setOrigin(0.5);
 
